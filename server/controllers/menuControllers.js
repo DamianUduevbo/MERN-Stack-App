9786a3e8a5762fbb942677fa8664fd48bf5a9a0f
Damian526 <58115973+Damian526@users.noreply.github.com>
2022-07-08 02:58:06 -0400

    CRUD for menu categories mostly completed.

--- server/controllers/menuControllers.js
+++ server/controllers/menuControllers.js
@@ -1,29 +1,65 @@
+const menuItemModel = require('../models/menuItemModel')
+const menuCatModel = require('../models/menuCategoryModel')
+
 const getCategories = async (req, res) => {
     try {
-        res.status(200).json({message: "Displays all categories"})
+        const category = await menuCatModel.find()
+        res.status(200).json(category)
     } catch {
         res.status(500).json({message: "505: Could not get all categories"})
     }
 }
 
+// TODO: check for repeats
 const addCategory = async (req, res) => {
+    if (!req.body.catName || !req.body.catItems) {
+        res.status(400)
+        throw new Error(`Category name and/or category items missing`)
+    }
+
     try {
-        res.status(200).json({message: `Add a category ${req.body}`})
+        const newCategory = await menuCatModel.create({
+            catName: req.body.catName,
+            catItems: req.body.catItems,
+        })
+
+        res.status(200).json(newCategory)
     } catch {
         res.status(400).json({message: `Cannot add the category ${req.body}`})
     }
 }
 
 const updateCategory = async (req, res) => {
+    const findCat = await menuCatModel.findById(req.params.id)
+
+    if (!findCat) {
+        res.status(404)
+        throw new Error(`Category name and/or category items missing`)
+    }
+
     try {
-        res.status(200).json({message: `Update this category: ${req.params.id}`})
+        const updatedCat = await menuCatModel.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        res.status(200).json(updatedCat)
     } catch {
         res.status(400).json({message: `Cannot update this category: ${req.params.id}`})
     }
 }
 
 const deleteCategory = async (req, res) => {
-    res.status(200).json({message: `Delete this category: ${req.params.id}`})
+    const findCat = await menuCatModel.findById(req.params.id)
+    
+    if (!findCat) {
+        res.status(404)
+        throw new Error(`Cannot update this category: ${req.params.id}`)
+    }
+
+    try {
+        //const updatedCat = await menuCatModel.findByIdAndDelete(req.params.id)
+        findCat.remove()
+        res.status(200).json({id: req.params.id})
+    } catch {
+        res.status(400).json({message: `Cannot update this category: ${req.params.id}`})
+    }
 }
 
 module.exports = {
